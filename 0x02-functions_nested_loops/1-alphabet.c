30d70d7c9c4d098a96a912ffda64b688b250756d
“Abraham <rajiabraham888@gmail.com>
2023-06-21 16:23:43 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Print lowercase alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
