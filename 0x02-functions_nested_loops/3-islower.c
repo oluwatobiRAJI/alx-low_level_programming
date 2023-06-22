72b874d1aab786361e8390a4ec8c30400d7da40c
“Abraham <rajiabraham888@gmail.com>
2023-06-21 17:04:27 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - Checks for lowercase character
+ *
+ * Return: 1 for lowercase character or 0 for otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
