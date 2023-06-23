1820dc63599df6bfebb2e2513417802eed53d261
“Abraham <rajiabraham888@gmail.com>
2023-06-23 13:50:42 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * mul - Multiplies two integers.
  * @a: The first number to multiply
  * @b: The second number to multiply with the first number
- * Return: The result of multiplication betweem a and b 
+ * Return: The result of multiplication betweem a and b
  */
 int mul(int a, int b)
 {
