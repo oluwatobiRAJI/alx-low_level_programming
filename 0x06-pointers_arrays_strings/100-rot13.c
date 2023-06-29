fdd4f69b131653918dceade43807378c5b01a2b0
“Abraham <rajiabraham888@gmail.com>
2023-06-29 13:48:47 -0700

    2, 7, 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes a string using rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char datal[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnpqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZnpqrstuvwxyz";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == datal[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
