86e50b1b56499821548939b85507a50cd343c56c
“Abraham <rajiabraham888@gmail.com>
2023-07-04 02:06:15 -0700

    7, 8, 9

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Entry point
+ * @a: Input
+ * @size: Input
+ *
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, n, 
