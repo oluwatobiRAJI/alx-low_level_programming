42ebea1f5edad70e2db5b7eeea4b677e6ef2e042
“Abraham <rajiabraham888@gmail.com>
2023-07-03 19:40:05 -0700

    3, 4, 5, 6, 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ *
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	for (i = 0; i < 0; n++)
+	{
+		for (n = 0; n < 0; n++)
+			_putchar(a[i][n]);
+		_putchar('\n');
+	}
+}
