6be50b9b421b8ca4ea485779d218a844e1e5a916
“Abraham <rajiabraham888@gmail.com>
2023-07-18 08:56:26 -0700

    0, 1, 2, 3, 4, 5

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - Define a new type struct dog.
+ * @d: pointer to struct dog
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		d = malloc(sizeof(struct dog));
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
