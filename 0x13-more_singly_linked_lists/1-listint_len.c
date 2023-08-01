47574b599fda18af1ef9c44053db478350729d6b
“Abraham <rajiabraham888@gmail.com>
2023-08-01 04:00:22 -0700

    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the number of elements in alinked lists
+ * @h: Linked list of type listint_t to traverse
+ *
+ * Return: Number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
